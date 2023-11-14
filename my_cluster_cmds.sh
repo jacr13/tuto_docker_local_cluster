472f122959c5a3cf9317c0b2c9b3fff2d6967e8c
João <joao.candido@etu.unige.ch>
2023-11-14 18:13:58 +0100

    Update my_cluster_cmds.sh

--- my_cluster_cmds.sh
+++ my_cluster_cmds.sh
@@ -34,137 +34,108 @@ alias my_report_percent='sreport user topusage start=2/16/22 end=2/23/24 -t perc
 
 alias my_squeue='squeue --format="%.10i %.20P %.50j %.10u %.2t %.10M %.4D %R" -u $USER --sort="t,-S" -u $USER'
 
-alias _squeue_helper='squeue -h --format="%.8i %30P %75j %.13u %.11L %.11l %.22S %.2t %.5D %.5C  %15R " --sort="t,-S" -u $USER'
+alias _squeue_helper='squeue -h --format="%.8i %150j %.13u %.11M %.11L %.11l %.2t" --sort="t,-S" -u $USER'
 
+alias mosh_kill='kill "pidof mosh-server"'
+
+alias srj='squeue -u $USER -t RUNNING'
+
+# squeue running count
+function src() {
+    N_JOBS_TOTAL=$(_squeue_helper | grep -E -c "$USER" )
+    N_JOBS_RUNNING=$(_squeue_helper | grep -wc R )
+    echo "Jobs running: $N_JOBS_RUNNING/$N_JOBS_TOTAL"
+}
 
 function my_squeue_gpu() {
    my_squeue | grep gpu
 }
 
 function ask_permission () {
-    echo "If you continue you will cancel $n_jobs/$n_total jobs..."
+    n_jobs_to_cancel="$1"
+    n_total="$2"
+    echo "If you continue you will cancel $n_jobs_to_cancel/$n_total jobs..."
     read -p "Do you still want to continue? [y/n]? " -r
 }
 
-function my_scancel_pattern() {
-    dryrun=1
-    if [ $# -eq 0 ]
-    then
-        echo "No arguments supplied... Try -h or --help to get help."
-        return
+function convert_to_seconds() {
+    local time=$1
+
+    # Check if the format includes days, hours, minutes, and seconds (d-hh:mm:ss)
+    if [[ $time == *-*:*:* ]]; then
+        # Extract time components with days
+        local days=$(echo "$time" | cut -d'-' -f1)
+        local hours=$(echo "$time" | awk -F ':' '{print $1}' | cut -d'-' -s -f2)
+        local minutes=$(echo "$time" | awk -F ':' '{print $2}')
+        local seconds=$(echo "$time" | awk -F ':' '{print $3}')
+    # Check if the format includes hours, minutes, and seconds (hh:mm:ss)
+    elif [[ $time == *:*:* ]]; then
+        # Extract time components without days
+        local days=0
+        local hours=$(echo "$time" | awk -F ':' '{print $1}')
+        local minutes=$(echo "$time" | awk -F ':' '{print $2}')
+        local seconds=$(echo "$time" | awk -F ':' '{print $3}')
+    # Check if the format includes minutes and seconds (mm:ss)
+    elif [[ $time == *:* ]]; then
+        # Extract time components without days and hours
+        local days=0
+	local hours=0
+        local minutes=$(echo "$time" | awk -F ':' '{print $1}')
+        local seconds=$(echo "$time" | awk -F ':' '{print $2}')
+    # Handle other formats or seconds-only case
+    else
+        ((total_seconds = time))
+        echo "$total_seconds"
+        return 0
     fi
-    while test $# -gt 0; do
-        case "$1" in
-            -h | --help)
-                echo "Usage: my_scancel [option...]" >&2
-                echo "   -h, --help             Show this help message"
-                echo "   -p, --pattern [text]   pattern to match on grep"
-                echo "   -nd, --no_dryrun       Doesn't show jobs in dry run mode (dangerous)"
-                echo ""
-                echo "This function will cancel all the jobs of USER that match pattern"
-                return
-                ;;
-            -p | --pattern)
-                shift
-                pattern=$1
-                shift
-                ;;
-            -nd | --no_dryrun)
-                dryrun=0
-                shift
-                ;;
-            *)
-            echo "$1 is not a recognized flag!"
-            return 1;
-            ;;
-        esac
-    done
-    n_jobs=$(_squeue_helper | grep -E -wc "$pattern")
-    n_total=$(_squeue_helper | grep -E -c "$USER" )
-    # jscpd:ignore-start
-    if [ $dryrun -eq 1 ]
-    then
-        echo "[DRYRUN] Number of jobs before cancel: $n_total"
-        echo "[DRYRUN] The following jobs are the ones that will be canceled!!"
-        _squeue_helper | grep -E "$pattern"
-        n_total=$(_squeue_helper | grep -E -c "$USER")
-        echo "[DRYRUN] Number of jobs after cancel: $((n_total - n_jobs))"
-    fi
-    
-    ask_permission
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        echo "Number of jobs before cancel: $n_total"
-        _squeue_helper | grep -E "$pattern" |  awk '{print $1}' | xargs scancel
-        echo "Waiting a bit for jobs to cancel..."
-        sleep 5
-        n_total=$(_squeue_helper | grep -E -c "$USER" )
-        echo "Number of jobs after cancel: $n_total"
-    fi
-    # jscpd:ignore-end
+
+    # Ensure values are in base-10 (decimal)
+    days=$((10#$days))
+    hours=$((10#$hours))
+    minutes=$((10#$minutes))
+    seconds=$((10#$seconds))
+
+    # Calculating the total seconds
+    total_seconds=$((days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds))
+
+    echo "$total_seconds"
 }
 
-function my_scancel_invert() {
-    dryrun=1
-    if [ $# -eq 0 ]
-    then
-        echo "No arguments supplied... Try -h or --help to get help."
-        return
-    fi
-    while test $# -gt 0; do
-        case "$1" in
-            -h | --help)
-                echo "Usage: my_scancel_invert [option...]" >&2
-                echo "   -h, --help             Show this help message"
-                echo "   -p, --pattern [text]   pattern to match on grep"
-                echo "   -nd, --no_dryrun       Doesn't show jobs in dry run mode (dangerous)"
-                echo ""
-                echo "This function will cancel all the jobs of USER that DO NOT match pattern"
-                return
-                ;;
-            -p | --pattern)
-                shift
-                pattern=$1
-                shift
-                ;;
-            -nd | --no_dryrun)
-                dryrun=0
-                shift
-                ;;
-            *)
-            echo "$1 is not a recognized flag!"
-            return 1;
-            ;;
-        esac
+function get_jobs_with_time() {
+    local output="$1"
+    local time=$(convert_to_seconds "$2")
+    local time_remaining="$3" # should be "0" or "1"
+
+    echo "$output" | while IFS= read -r line; do
+        # Extract relevant columns
+        job_id=$(echo "$line" | awk '{print $1}')
+        job_state=$(echo "$line" | awk '{print $7}')
+        job_duration=$(echo "$line" | awk '{print $4}')
+        job_duration_seconds=$(convert_to_seconds "$job_duration")
+        job_remaining=$(echo "$line" | awk '{print $5}')
+        job_remaining_seconds=$(convert_to_seconds "$job_remaining")
+
+        # Check conditions and perform actions
+        if [ "$job_state" == "R" ]; then
+            if [ $time_remaining -eq 0 ]; then
+                if [ "$job_duration_seconds" -gt "$time" ]; then
+                    echo "$line"
+                fi
+            else
+                if [ "$job_remaining_seconds" -gt "$time" ]; then
+                    echo "$line"
+                fi
+            fi
+        fi
     done
-    n_jobs=$(_squeue_helper | grep -E -v -c "$pattern")
-    n_total=$(_squeue_helper | grep -E -c "$USER" )
-    # jscpd:ignore-start
-    if [ $dryrun -eq 1 ]
-    then
-        echo "[DRYRUN] Number of jobs before cancel: $n_total"
-        echo "[DRYRUN] The following jobs are the ones that will REMAIN all the other jobs will be canceled!!"
-        _squeue_helper | grep -E "$pattern"
-        n_total=$(_squeue_helper | grep -E -c "$USER")
-        echo "[DRYRUN] Number of jobs after cancel: $((n_total - n_jobs))"
-    fi
-    
-    ask_permission
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        echo "Number of jobs before cancel: $n_total"
-        _squeue_helper | grep -E -v "$pattern" |  awk '{print $1}' | xargs scancel
-        echo "Waiting a bit for jobs to cancel..."
-        sleep 5
-        n_total=$(_squeue_helper | grep -E -c "$USER" )
-        echo "Number of jobs after cancel: $n_total"
-    fi
-    # jscpd:ignore-end
 }
 
 function my_scancel() {
     dryrun=1
-    invert=0
+    pattern=""
+    inverse_pattern=0
+    time=""
+    inverse_time=0
     if [ $# -eq 0 ]
     then
         echo "No arguments supplied, this command will cancel ALL your jobs!"
@@ -180,13 +151,42 @@ function my_scancel() {
     while test $# -gt 0; do
         case "$1" in
             -h | --help)
-                echo "Usage: my_scancel_invert [option...]" >&2
+                echo "Usage: my_scancel [option...]" >&2
                 echo "   -h, --help             Show this help message"
-                echo "   -p, --pattern [text]   pattern to match on grep (will cancel all jobs that match this pattern)"
-                echo "   -i, --invert           invert the pattern, so now this command will KEEP jobs that match the pattern"
-                echo "   -nd, --no_dryrun       Doesn't show jobs in dry run mode (dangerous)"
+                echo "   -p, --pattern [text]   Pattern to match using grep (cancels all jobs that match this pattern)"
+                echo "   -ip, --inverse_pattern Invert the pattern, so now the command will KEEP jobs that match the pattern"
+                echo "   -t, --time [time]      Stop jobs with execution time greater than this value (d-hh:mm:ss, hh:mm:ss, mm:ss, s or #d, #h, #m)"
+                echo "   -it, --inverse_time    Stop jobs with remaining time greater than the time passed with --time"
+                echo "   -nd, --no_dryrun       Don't show jobs in dry run mode (dangerous)"
                 echo ""
-                echo "This command will cancel all the jobs of USER that match pattern if neither -i or --invert are specified, and cancel all jobs that DO NOT match the pattern if -i or --invert are specified!"
+                echo "Description:"
+                echo "   This fundtion provides a flexible way to cancel jobs on a cluster based on different criteria."
+                echo "   Use the options to specify patterns, time limits, and dry run behavior."
+                echo ""
+                echo "Examples:"
+                echo "   1. Cancel all jobs that contain the pattern 'experiment':"
+                echo "      my_scancel -p experiment"
+                echo ""
+                echo "   2. Cancel all jobs that DON'T contain the pattern 'experiment':"
+                echo "      my_scancel -p experiment -ip"
+                echo ""
+                echo "   3. Cancel jobs with execution time exceeding 1 hour:"
+                echo "      my_scancel -t 01:00:00"
+                echo "      my_scancel -t 1h"
+                echo ""
+                echo "   4. Cancel jobs with remaining time exceeding than 2 days:"
+                echo "      my_scancel -t 2-00:00:00 -it"
+                echo "      my_scancel -t 2d -it"
+                echo ""
+                echo "   5. Cancel all jobs:"
+                echo "      5.1 with a dry run preview:"
+                echo "          my_scancel"
+                echo "      5.2 without displaying a dry run preview:"
+                echo "          my_scancel -nd"
+                echo ""
+                echo "   6. It is possible to cancel jobs with pattern and time constrain:"
+                echo "      my_scancel -p pattern -t 2d"
+                echo "      my_scancel -p pattern -ip -t 2d -it"
                 return
                 ;;
             -p | --pattern)
@@ -194,8 +194,17 @@ function my_scancel() {
                 pattern=$1
                 shift
                 ;;
-            -i | --invert)
-                invert=1
+            -ip | --inverse_pattern)
+                inverse_pattern=1
+                shift
+                ;;
+            -t | --time)
+                shift
+                time=$1
+                shift
+                ;;
+            -it | --inverse_time)
+                inverse_time=1
                 shift
                 ;;
             -nd | --no_dryrun)
@@ -209,16 +218,58 @@ function my_scancel() {
         esac
     done
 
-    dryrun_params=""
-    if [ $dryrun -eq 0 ]
-    then
-        dryrun_params="--no_dryrun"
+    # if pattern is specified, get jobs with pattern
+    if [ -n "$pattern" ]; then
+        # Define the grep parameters
+        grep_params="-E"
+        if [ $inverse_pattern -eq 1 ]
+        then
+            grep_params="-v $grep_params"
+        fi
+
+        # get jobs that match pattern or the ones that do NOT match the pattern
+        jobs_to_cancel=$(_squeue_helper | grep ${grep_params} "$pattern")
+    else
+        jobs_to_cancel=$(_squeue_helper)
     fi
 
-    if [ $invert -eq 1 ]
+    # if time is specified, get jobs that match time requirements
+    if [ -n "$time" ]; then
+        jobs_to_cancel=$(get_jobs_with_time "$jobs_to_cancel" "$time" "$inverse_time")
+    fi
+
+    n_total=$(_squeue_helper | grep -wc "$USER")
+    n_jobs_to_cancel=$(echo "$jobs_to_cancel" | grep -wc "$USER")
+
+    if [ $dryrun -eq 1 ]
     then
-        my_scancel_invert -p $pattern $dryrun_params
-    else
-        my_scancel_pattern -p $pattern $dryrun_params
+        echo "[DRYRUN] Number of jobs before cancel: $n_total"
+        if [ $inverse_pattern -eq 1 ]
+        then
+            echo "[DRYRUN][INVERSE] The following jobs are the ones that will REMAIN all the other jobs will be canceled!!"
+        else
+            echo "[DRYRUN] The following jobs are the ones that WILL be killed"
+        fi
+        echo ""
+        if [ -n "$time" ]; then
+            jobs_tmp=$(_squeue_helper | grep -E "$pattern")
+            get_jobs_with_time "$jobs_tmp" "$time" "$inverse_time"
+        else
+            _squeue_helper | grep -E "$pattern"
+        fi
+        echo "[DRYRUN] Number of jobs after cancel: $((n_total - n_jobs_to_cancel))/$n_total"
+    fi
+    echo ""
+
+    ask_permission "$n_jobs_to_cancel" "$n_total"
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo ""
+        echo "Canceling jobs..."
+        echo "$jobs_to_cancel" |  awk '{print $1}' | xargs scancel
+        echo "Waiting a bit for jobs to cancel..."
+        sleep 5
+        n_total=$(_squeue_helper | grep -E -c "$USER" )
+        echo "Number of jobs after cancel: $n_total"
     fi
 }
